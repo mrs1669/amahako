f55867c4d7fd81f54c5372ef31d123542f7ce061
mrs1669 <mrs1669.develop@gmail.com>
2019-06-03 17:54:04 +0900

    today's works

---
+++ amahako-2019-06-03/NewsReader/NewsReader/ListVIewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListVIewController.swift
+//  NewsReader
+//
+//  Created by Takumi Muraishi on 2019/06/03.
+//  Copyright © 2019 Takumi Muraishi. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+}
+
+
